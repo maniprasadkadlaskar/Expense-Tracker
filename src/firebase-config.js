49332062d079441c3c5457e545196dc35496553f
Maniprasad Kadlaskar <maniprasadvkadlaskar@gmail.com>
2023-05-31 22:36:15 +0530

    bug-fix

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from '@firebase/firestore'
 import { getAuth } from 'firebase/auth'
-require('dotenv').config()
 
 // App Firebase configuration
 const firebaseConfig = {
